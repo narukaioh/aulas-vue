d01fe8a44a49f27ca56499a48f7c21ac4c672704
Ju Dantas <jucienyds@gmail.com>
2017-05-20 08:33:18 -0300

    testando as rotas

--- webproject/src/App.vue
+++ webproject/src/App.vue
@@ -1,6 +1,13 @@
 <template lang="pug">
   #app
-    p {{ msg }}
+    form(method="post", action="#")
+      .row.uniform
+        div(class="6u 12u$(xsmall)")
+          input(type="text", name="name", id="name", placeholder="Name")
+        div(class="6u$ 12u$(xsmall)")
+          input(type="password", name="password", id="password", placeholder="Password")
+        div(class="12u$")
+          input(type="text", name="email", id="email", placeholder="Email")
 </template>
 
 <script>
@@ -21,7 +28,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 980px;
+  margin: 60px auto;
 }
 
 h1, h2 {
--- webproject/src/main.js
+++ webproject/src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
 import App from './App.vue'
+import About from './Login.vue'
+
+
+const router = new VueRouter({
+	mode: 'history',
+	base: '__dirname',
+	routes: [
+		{ path: '/about', component: About }
+	]
+})
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
