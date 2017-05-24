7b51856bf6774f56a436083770e3a4def7ccef23
Ju Dantas <jucienyds@gmail.com>
2017-05-24 10:19:15 -0300

    criada as funcoes para os servicos

--- webproject/src/modules/service.js
+++ webproject/src/modules/service.js
@@ -1,5 +1,7 @@
 import Http from './client' 
 
+// users
+
 const addUser = user => {
 	return Http.post('/users', user).then(res => res.data)
 }
@@ -8,12 +10,42 @@ const listUsers = () => {
 	return Http.get('/users').then(res => res.data)
 }
 
-const listCategories = () => {
-	return Http.get('/categories').then(res => res.data)
-}
-
 const removeUser = id => {
 	return Http.delete('/users/'+id).then(res => res.data )
 }
 
-export { addUser, listUsers, listCategories, removeUser }
+const editUser = id => {
+	return Http.put('/users/'+id).then(res => res.data )	
+}
+
+// categories
+const addCategory = category => {
+	return Http.post('/categories', category).then(res => res.data)
+}
+const listCategories = () => {
+	return Http.get('/categories').then(res => res.data)
+}
+const removeCategory = id => {
+	return Http.delete('/categories/'+id).then(res => res.data )
+}
+
+const editCategory = id => {
+	return Http.put('/categories/'+id).then(res => res.data )	
+}
+
+// articles
+const addArticle = article => {
+	return Http.post('/articles', article).then(res => res.data)
+}
+const listArticles = () => {
+	return Http.get('/articles').then(res => res.data)
+}
+const removeArticle = id => {
+	return Http.delete('/articles/'+id).then(res => res.data )
+}
+
+const editArticle = id => {
+	return Http.put('/articles/'+id).then(res => res.data )	
+}
+
+export { addUser, listUsers, removeUser, editUser, addCategory, listCategories, removeCategory, editCategory, addArticle, listArticles, removeArticle, editArticle }
