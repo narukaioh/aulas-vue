b990bd83b68139ff9cd3b1938f711318b4c54207
Ju Dantas <jucienyds@gmail.com>
2017-07-13 00:11:29 -0300

    Revert "deu errado"
    This reverts commit 4d0a09ff6d8bc170435f030c0b53001c6f9d997d.

--- webproject/src/components/Home.vue
+++ webproject/src/components/Home.vue
@@ -3,8 +3,6 @@ div.inner
 	top
 	main-article
 	portfolio
-	quill
-
 </template>
 
 <script>
@@ -13,15 +11,13 @@ import Header from './elements/common/Header.vue'
 import MainArticle from './elements/home/MainArticle.vue'
 import Features from './elements/home/Features.vue'
 import Portfolio from './elements/home/Portfolio.vue'
-import { quillEditor } from 'vue-quill-editor'
 
 export default {
 	components: {
 		'top': Header,
 		'main-article': MainArticle,
 		'features': Features,
-		'portfolio': Portfolio,
-		'quill': quillEditor
+		'portfolio': Portfolio
 	}
 }
 
--- webproject/src/main.js
+++ webproject/src/main.js
@@ -6,7 +6,6 @@ import store from './store/store'
 import router from './router'
 import App from './App.vue'
 
-
 Vue.use(VueRouter)
 
 new Vue({
